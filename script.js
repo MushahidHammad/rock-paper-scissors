d2172d70dbd8be9621f6e5ef5a293cf843754e59
mushahid <mushahidhammadabbasi@gmail.com>
2024-12-04 17:27:28 +0500

    Project completed

---
+++ script.js
@@ -0,0 +1,64 @@
+function getComputerChoice() {
+    let x = Math.random();
+    if (x >= 0 && x <= 0.33)
+        return "Rock";
+    else if (x > 0.33 && x <= 0.66)
+        return "Paper";
+    else 
+        return "Scissors";
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter rock, paper or scissors:");
+    if(["rock", "paper", "scissors"].includes(choice)) {
+        return choice;
+    } else {
+        alert("Invalid choice, try again");
+        return getHumanChoice();
+    }
+}
+    
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const winMessage = `You win! ${humanChoice} beats ${computerChoice}`;
+    const loseMessage = `You lose! ${computerChoice} beats ${humanChoice}`;
+    const tieMessage = "It's a tie!";
+
+    if(humanChoice === computerChoice) {
+        console.log(tieMessage);
+    }
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++;
+        console.log(winMessage);
+    }
+    else {
+        computerScore++
+        console.log(loseMessage);
+    }
+}
+
+
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You won the game! Final score: ${humanScore}-${computerScore}`);
+    } else if (humanScore < computerScore) {
+        console.log(`you lost the game! Final score: ${humanScore}-${computerScore}`);
+    } else {
+        console.log(`It's a tie. Final score: ${humanScore}-${computerScore}`);
+    }
+}
+
+playGame();
+    
